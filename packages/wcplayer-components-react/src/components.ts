18885282024b483f97da53b606005e15ba8cba30
iChengbo <chengbo_x@163.com>
2022-08-05 16:04:55 +0800

    feat(@wcplayer/components-react): new package

---
+++ packages/wcplayer-components-react/src/components.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@wcplayer/components';
+
+import { defineCustomElements } from '@wcplayer/components/loader';
+
+defineCustomElements();
+export const WcControls = /*@__PURE__*/createReactComponent<JSX.WcControls, HTMLWcControlsElement>('wc-controls');
+export const WcCoverLayer = /*@__PURE__*/createReactComponent<JSX.WcCoverLayer, HTMLWcCoverLayerElement>('wc-cover-layer');
+export const WcFullscreen = /*@__PURE__*/createReactComponent<JSX.WcFullscreen, HTMLWcFullscreenElement>('wc-fullscreen');
+export const WcLayers = /*@__PURE__*/createReactComponent<JSX.WcLayers, HTMLWcLayersElement>('wc-layers');
+export const WcPictureInPicture = /*@__PURE__*/createReactComponent<JSX.WcPictureInPicture, HTMLWcPictureInPictureElement>('wc-picture-in-picture');
+export const WcPlayPause = /*@__PURE__*/createReactComponent<JSX.WcPlayPause, HTMLWcPlayPauseElement>('wc-play-pause');
+export const WcPlayPauseLayer = /*@__PURE__*/createReactComponent<JSX.WcPlayPauseLayer, HTMLWcPlayPauseLayerElement>('wc-play-pause-layer');
+export const WcPlayer = /*@__PURE__*/createReactComponent<JSX.WcPlayer, HTMLWcPlayerElement>('wc-player');
+export const WcProgress = /*@__PURE__*/createReactComponent<JSX.WcProgress, HTMLWcProgressElement>('wc-progress');
+export const WcSpacer = /*@__PURE__*/createReactComponent<JSX.WcSpacer, HTMLWcSpacerElement>('wc-spacer');
+export const WcVideo = /*@__PURE__*/createReactComponent<JSX.WcVideo, HTMLWcVideoElement>('wc-video');
+export const WcVolume = /*@__PURE__*/createReactComponent<JSX.WcVolume, HTMLWcVolumeElement>('wc-volume');
--- packages/wcplayer-components/stencil.config.ts
+++ packages/wcplayer-components/stencil.config.ts
@@ -1,9 +1,15 @@
 import { Config } from '@stencil/core';
 import { vueOutputTarget } from '@stencil/vue-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'wcplayer',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@wcplayer/components',
+      proxiesFile: '../wcplayer-components-react/src/components.ts',
+      includeDefineCustomElements: true,
+    }),
     vueOutputTarget({
       componentCorePackage: '@wcplayer/components',
       proxiesFile: '../wcplayer-components-vue/src/components.ts',
