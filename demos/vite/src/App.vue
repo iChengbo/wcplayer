5ad11462b69046b93d0e35fd647ea8de66f4d6fd
iChengbo <chengbo_x@163.com>
2022-08-11 09:18:38 +0800

    fix: demos/vite don't work

--- demos/vite/src/App.vue
+++ demos/vite/src/App.vue
@@ -1,21 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
 import { WcPlayer } from '@wcplayer/components-vue'
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
-  <div style="height: 720px;width: 1280px;">
+  <div class="container" style="height: 720px;width: 1280px;">
     <WcPlayer muted autoplay controls src="https://d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4" />
   </div>
 </template>
@@ -25,15 +15,8 @@ import { WcPlayer } from '@wcplayer/components-vue'
   margin: 0;
   padding: 0;
 }
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.container {
+  height: 720px;
+  width: 1280px;
 }
 </style>
--- demos/vite/vite.config.js
+++ demos/vite/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@wcplayer/components-vue': resolve(__dirname, '../../packages/wcplayer-components-vue/src/index.ts'),
+    },
+  },
 })
